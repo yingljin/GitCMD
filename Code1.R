f51770c13ee1351e9d2a313f52a2df0d5591a7e6
yingljin <ying.jin@cuanschutz.edu>
2023-03-18 12:32:54 -0600

    edit code1 in testing branch

--- Code1.R
+++ Code1.R
@@ -1,4 +1,4 @@
-df <- data.frame(rnorm(20), rnorm(20), rnorm(20), rnorm(20))
+df <- data.frame(rnorm(10), rnorm(10), rnorm(10), rnorm(10))
 
 df2 <- data.frame(y = rbind(15))
 save(df, file = "df.RData")
