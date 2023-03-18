0513ccf100fd7feeecaf53b6b6aad277350b160b
yingljin <ying.jin@cuanschutz.edu>
2023-03-18 12:33:36 -0600

    edit code1 in master branch

--- Code1.R
+++ Code1.R
@@ -1,4 +1,4 @@
-df <- data.frame(rnorm(20), rnorm(20), rnorm(20), rnorm(20))
+df <- data.frame(rnorm(30), rnorm(30), rnorm(30), rnorm(30))
 
 df2 <- data.frame(y = rbind(15))
 save(df, file = "df.RData")
